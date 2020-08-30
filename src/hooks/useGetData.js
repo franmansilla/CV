ee85994e4408c8e0545753fed5cdfbe40bb6b24d
franmansilla <40762152+franmansilla@users.noreply.github.com>
2020-08-29 21:21:11 -0300

    Update useGetData.js

--- src/hooks/useGetData.js
+++ src/hooks/useGetData.js
@@ -1,5 +1,5 @@
 import {useState, useEffect} from 'react';
-const api='https://franmansilla.github.io/api-json/db.json';
+const api='https://franmansilla.github.io/api-json/api.json';
 const useGetData =()=> {
     const [mydata, setdata]= useState([]);
 
